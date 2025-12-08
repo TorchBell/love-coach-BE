e53473b0ebc799a1c8b12eaf66863afe8dcb9fd3
kbs <kim3210542@gmail.com>
2025-12-09 00:30:43 +0900

    initial commit

---
+++ src/test/java/com/torchbell/lovecoach/LoveCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torchbell.lovecoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

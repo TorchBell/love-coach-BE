a405c3044960c2965bc5c77b4ee0b94940b95d13
kbs <kim3210542@gmail.com>
2025-12-09 00:30:43 +0900

    initial commit

---
+++ src/main/java/com/torchbell/lovecoach/LoveCoachApplication.java
@@ -0,0 +1,13 @@
+package com.torchbell.lovecoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveCoachApplication.class, args);
+    }
+
+}
